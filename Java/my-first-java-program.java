4e7f336f2eb983af90e7907c5d8907862e8247e2
Juan C. Gonzalez <33580723+jsdev17@users.noreply.github.com>
2017-11-21 00:36:05 -0500

    Create my-first-java-program.java

---
+++ Java/my-first-java-program.java
@@ -0,0 +1,9 @@
+//Jan. 26, 2013
+//This is my first Java Program.
+public class MyFirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
