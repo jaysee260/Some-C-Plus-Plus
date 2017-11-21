c302257d7a9853396488719e493bad0161d2bfac
Juan C. Gonzalez <33580723+jsdev17@users.noreply.github.com>
2017-11-21 00:15:06 -0500

    Create c-plus-plus.cpp

---
+++ C-Plus-Plus/c-plus-plus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main ()
+{
+    cout << "  CCCC    +        + \n";
+    cout << " C        +        + \n";
+    cout << "C      +++++++  +++++++  \n";
+    cout << " C        +        + \n";
+    cout << "  CCCC    +        +  \n";        
+     
+    system ("pause");
+    return 0;
+     
+}
