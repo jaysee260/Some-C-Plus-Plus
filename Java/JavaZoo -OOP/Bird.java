08dd2035477b2d9b8d053442539776508b6f96d8
Juan C. Gonzalez <33580723+jsdev17@users.noreply.github.com>
2017-11-21 00:46:58 -0500

    Create Bird.java

---
+++ Java/JavaZoo -OOP/Bird.java
@@ -0,0 +1,40 @@
+public class Bird extends ZooAnimal
+{
+    //attributes
+    protected double wingspan;
+    protected boolean buildNests;
+     
+    //constructors
+    public Bird()
+    {
+        wingspan = 0.0;
+        buildNests = true;
+        name = "anonymous bird";
+        type = "unknown bird";
+        System.out.println("Bird's default constructor invoked.");
+    }
+     
+    public Bird(double w, boolean n)
+    {
+        wingspan = w;
+        buildNests = n;
+        System.out.println("Bird's 2-arg constructor is invoked."); 
+    }
+     
+    public Bird(String n, String t, int a, boolean h, double w, boolean ne)
+    {
+        super(n, t, a, h);
+        wingspan = w;
+        buildNests = ne;
+        System.out.println("Bird's 6-arg constructor is invoked.");
+    }
+     
+    //methods
+    public void layEggs()
+    {
+        System.out.println(name + " is laying eggs.");
+    }
+ 
+ 
+ 
+}//end class
