8b0bfbb543632925796f172d6e0d6de77df6a97f
Juan C. Gonzalez <33580723+jsdev17@users.noreply.github.com>
2017-11-21 00:14:37 -0500

    Create first-program.cpp
    the first program I ever wrote...

---
+++ C-Plus-Plus/first-program.cpp
@@ -0,0 +1,12 @@
+//01-12-2012
+#include <iostream>
+using namespace std;
+ 
+int main ()
+{
+    cout <<"Welcome to C++ \n";
+    cout <<"Welcome to Computer Science \n";
+    cout <<"Programming is fun \n" ;
+    system("pause");
+    return 0;
+}
